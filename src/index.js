62954e1de5b64ba5c8e92fb1cca3acd60f25d327
Sean Whalley <swhalley@gmail.com>
2017-03-26 00:26:35 -0300

    readded some fake data to check performance.

--- src/index.js
+++ src/index.js
@@ -36,12 +36,12 @@ class Root extends React.Component {
             "Anibal Gholson", "Valeria Villalon", "Ozella Wymer", "Lyndsey Rounds", "Gussie Bustamante",
             "Niki Hetzler", "Marlyn Rey", "Lupe Burke", "Jacinto Casarez", "Cordell Courville",
             "Angelena Sickels", "Talia Harty", "Bret Califano", "David Davies", "Lan Barre",
-            //"Refugio Hopp", "Marquetta Witherington", "Carlita Stecklein", "Will Armentrout", "Birgit Kerner",
-            //"Elmira Lemke", "Tasha Stefaniak", "Golda Roeder", "Federico Shuck", "Rodger Culbertson",
-            //"Victorina Brochu", "Arie Laboy", "Peggy Morvant", "Sharyl Nott", "Josephine Deno",
-            //"Elenor Buel", "Julee Veiga", "Edgardo Disney", "Agueda Bregman", "Carmella Moriarity",
-            //"Erline Karr", "Janean Harbour", "Altagracia Kushner", "Hazel Masker", "Hobert Cid",
-            //"Tegan Sidhu", "Izetta Jacome", "Camie Repp", "Virgie Ely", "Frieda Dionne"
+            "Refugio Hopp", "Marquetta Witherington", "Carlita Stecklein", "Will Armentrout", "Birgit Kerner",
+            "Elmira Lemke", "Tasha Stefaniak", "Golda Roeder", "Federico Shuck", "Rodger Culbertson",
+            "Victorina Brochu", "Arie Laboy", "Peggy Morvant", "Sharyl Nott", "Josephine Deno",
+            "Elenor Buel", "Julee Veiga", "Edgardo Disney", "Agueda Bregman", "Carmella Moriarity",
+            "Erline Karr", "Janean Harbour", "Altagracia Kushner", "Hazel Masker", "Hobert Cid",
+            "Tegan Sidhu", "Izetta Jacome", "Camie Repp", "Virgie Ely", "Frieda Dionne"
         ];
 
         people.forEach( (person) => {
